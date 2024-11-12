3b71b3892dcf26336ac210fa0c1b640bd166eae9
Vivek Verma <vivek17102005@gmail.com>
2024-11-12 08:59:16 +0530

    counter project

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+    // this wont work because the counter is a normal variable
+    // let counter = 10;
+
+    //as react handels the UI part, the variable should be created using react's library
+    //so the variable is constantly reflected in the UI
+    let [counter, setCounter] = useState(10);
+
+    function increase() {
+        if (counter < 20) setCounter(counter + 1);
+    }
+    function decrease() {
+        if (counter > 0) setCounter(counter - 1);
+    }
+
+    return (
+        <>
+            <h1>Counter🔄</h1>
+            <h2>Current value is : {counter}</h2>
+            <button onClick={increase}>Increase</button>
+            <button onClick={decrease}>Decrease</button>
+        </>
+    );
+}
+
+export default App;
